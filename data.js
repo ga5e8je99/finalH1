7ade788925b650c186d0ddb150e97e1028186907
gamal-ahmed-306 <127439005+ga5e8je99@users.noreply.github.com>
2024-12-20 13:12:49 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,121 @@
+var hotels = {
+    "hotel1": {
+      "name": "AC Hotel by Marriott Krakow",
+      "location": "miami",
+      "rating": "★★★★★",
+      "roomNumber": 10,
+      "map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2561.2961348431627!2d19.910359959672004!3d50.06201547163911!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47165be593bfc517%3A0x964d597fdda1e555!2sAC%20Hotel%20by%20Marriott%20Krakow!5e0!3m2!1sar!2seg!4v1734160178117!5m2!1sar!2seg" 
+      ,
+      "image": "https://lh3.googleusercontent.com/proxy/QXCMVFCaIFBghsspF7NNx0muQqac82eVjcZDlbBkvrc_d2ptkaa94Kc-0k2WtGI_jQBESDXvT4Yj4bA6IOIHgK3U5MHfQMNbgXf6nK261RgKm3gWzuNZcFeXje8fR2rGYUWLL9UmMIWGfPKAdETx4p2CXvq5sA=s1360-w1360-h1020",
+      "description": "This hotel is located near the Miami Beach Boardwalk . ",
+      "rooms": {
+        "room1": { "bedSize": "Deluxe Room", "maxPepole": "single", "price": 100, "option": "Wi-Fi, King Rooms", "image": "https://cache.marriott.com/content/dam/marriott-renditions/MIAAC/miaac-guestroom-0033-hor-clsc.jpg?output-quality=90&interpolation=progressive-bilinear&downsize=562px:366px"},
+        "room2": { "bedSize": "Deluxe Room", "maxPepole": "double", "price": 150, "option": "Wi-Fi, Ocean View", "image": "https://cache.marriott.com/content/dam/marriott-renditions/MIAAC/miaac-suite-0040-hor-clsc.jpg?output-quality=90&interpolation=progressive-bilinear&downsize=562px:366px" }
+      }
+    },
+    "hotel2": {
+      "name": "Capital Hilton",
+      "location": "New York City",
+      "rating": "★★★★",
+      "roomNumber": 10,
+      "map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3104.9079797254844!2d-77.03314217499226!3d38.90321957172277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7b7be57f45d79%3A0xe54ae1a5d8eab767!2z2YPYp9io2YrYqtmI2YQg2YfZitmE2KrZiNmG!5e0!3m2!1sar!2seg!4v1734453849167!5m2!1sar!2seg",
+      "image": "https://www.hilton.com/im/en/DCASHHH/17157386/exterior.jpeg?impolicy=crop&cw=5000&ch=2799&gravity=NorthWest&xposition=0&yposition=385&rw=768&rh=430",
+      "description": "A luxury hotel in New York City.",
+      "rooms": {
+        "room1": { "bedSize": "Suite", "maxPepole": "double", "price": 250, "option": "Wi-Fi, Ocean View", "image": "https://www.hilton.com/im/en/DCASHHH/19876312/k1-1.jpg?impolicy=crop&cw=5000&ch=2799&gravity=NorthWest&xposition=0&yposition=269&rw=768&rh=430" }
+      }
+    },
+    "hotel3": {
+      "name": "The Biltmore ",
+      "location": "Los Angeles",
+      "rating": "★★★★",
+      "roomNumber": 50,
+       "map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6785587.465852451!2d-110.53981080304843!3d33.8567996862616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c7b4a08146a5%3A0x1f204b38fe4de173!2z2YXZitmE2YrZhtmK2YjZhSDYqNin2YTYqtmK2YXZiNixINmE2YjYsyDYp9mG2KzZhNmK2LM!5e0!3m2!1sar!2seg!4v1734477891734!5m2!1sar!2seg",
+      "image": "https://lh3.googleusercontent.com/p/AF1QipMaPE5403svGQZqBcHn6jQnZ3KHnEnm74qBZYS_=s1360-w1360-h1020",
+      "description": "A luxury hotel in Los Angeles.",
+      "rooms": {
+        "room1": { "bedSize": "Suite", "maxPepole": "double", "price": 250, "option": "Wi-Fi, Ocean View", "image": "https://lh3.googleusercontent.com/proxy/a6W8BIq57Kf51bg6V-gazW_4VI6l8LVIh3pA7yo14F1Ba38kvTHZv4EmmVh5i3ls7HESrHy0uJeuGffWAWqGOraZlfk6doHIVQeFWBBub_hPoxn_NtlIFhyzVvZDoUmGk18XRcuajUk9i75Q16it_LqsCZfc4cc=s1360-w1360-h1020" }
+      }
+    },
+    "hotel4": {
+      "name": "The Langham ",
+      "location": "New York City",
+      "rating": "★★★★★",
+      "roomNumber": 50,
+       "map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.7396098542345!2d-0.1440438!3d51.517993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761ad507f97e8f%3A0xc5f41a1596b6192!2z2LDYpyDZhNin2YbYutmH2KfZhSDZhNmG2K_Zhg!5e0!3m2!1sar!2seg!4v1734478194495!5m2!1sar!2seg",
+      "image": "https://lh3.googleusercontent.com/p/AF1QipP32-7443rCByy9UjAAne5KT21MksEKzHBo7m6l=s1360-w1360-h1020",
+      "description": "A luxury hotel in New York City.",
+      "rooms": {
+        "room1": { "bedSize": "Suite", "maxPepole": "double", "price": 250, "option": "Wi-Fi, Ocean View", "image": "https://lh3.googleusercontent.com/p/AF1QipNZjfPLC-Fq4Wbpyf1uykac4jo-uMcMoIUoP0DP=s1360-w1360-h1020" },
+        "room2": { "bedSize": "Deluxe Room", "maxPepole": "triple", "price": 350, "option": "Wi-Fi, Ocean View", "image": "https://lh3.googleusercontent.com/p/AF1QipNimr4E1CN3wrmA58cegO_FSmjHI0uA_g8_0jvD=s1360-w1360-h1020"}
+      }
+    },
+    "hotel5": {
+      "name": "Novotel Miami Brickell ",
+      "location": "miami",
+      "rating": "★★★",
+      "roomNumber": 10,
+       "map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3593.3262942920182!2d-80.19434817449402!3d25.759783877353115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9b687b40a7671%3A0xf4856d1a9bd78c42!2sNovotel%20Miami%20Brickell!5e0!3m2!1sar!2seg!4v1734478369354!5m2!1sar!2seg",
+      "image": "https://lh3.googleusercontent.com/p/AF1QipPptgnqEK2LT82TkVTzKGfcgcqI3XvP8SidAAHz=s1360-w1360-h1020",
+      "description": "A beautful hotel in miami.",
+      "rooms": {
+        "room1": { "bedSize": "Suite", "maxPepole": "single", "price": 150, "option": "Wi-Fi, Ocean View", "image": "https://lh3.googleusercontent.com/p/AF1QipPOoT80Xed2gY-bM-i0odiZ_AdgYMgCofY1Tq0o=s1360-w1360-h1020" },
+        "room2": { "bedSize": "Deluxe Room", "maxPepole": "double", "price": 250, "option": "Wi-Fi, Ocean View", "image": "https://lh3.googleusercontent.com/p/AF1QipP2nQMi4frkMGhiQGrfCSYCZ3xtymjDQKGIqlxB=s1360-w1360-h1020"}
+       }
+      }, 
+      "hotel6": {
+      "name": "Kimpton EPIC Hotel",
+      "location": "miami",
+      "rating": "★★★★",
+      "roomNumber": 10,
+       "map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3593.0012737486395!2d-80.18676963894278!3d25.770521477440667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9b69d1a756195%3A0x27fd2dc0fcef0f92!2sKimpton%20EPIC%20Hotel!5e0!3m2!1sar!2seg!4v1734157653253!5m2!1sar!2seg",
+      "image": "https://lh3.googleusercontent.com/proxy/NBqCYHh_cm3WtZFNMcpSOXwlS8Q_mYpSYZXV_jPps-ndgY981-bLzMv3JzQra9_5J371BuUrdXn3rMCwG23RP6Jhe0JkxvFHyM7MVhOojRSAB2ICC6qsiJ-wMpbnFqU4ApYJagcufezqSbpA5RKTN9nogzHDK2htsr-dHg=s1360-w1360-h1020",
+      "description": "A luxury hotel in miami.",
+      "rooms": {
+        "room1": { "bedSize": "Suite", "maxPepole": "single", "price": 250, "option": "Wi-Fi, Ocean View", "image": "https://lh3.googleusercontent.com/p/AF1QipMcq7ZseGk2IM3niPTKlidARuNhlaPjJf1jZde3=s1360-w1360-h1020" },
+        "room2": { "bedSize": "Deluxe Room", "maxPepole": "double", "price": 350, "option": "Wi-Fi, Ocean View", "image": "https://lh3.googleusercontent.com/p/AF1QipM9ebMVeCX7kQ8-2HE6lzerr_wVymalBIqKDaTx=s1360-w1360-h1020"}
+       }
+      },
+       "hotel7": {
+      "name": "Waldorf Astoria ",
+      "location": "New York city",
+      "rating": "★★★",
+      "roomNumber": 10,
+       "map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.2628917283405!2d-73.97028097526565!3d40.75624227138689!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c258fc55ba384f%3A0x7ebcd3b680ebd80e!2sWaldorf%20Astoria%20New%20York!5e0!3m2!1sar!2seg!4v1734479085655!5m2!1sar!2seg",
+      "image": "https://lh3.googleusercontent.com/p/AF1QipNKy6oaOw7aYwH-YgcswvYcxY12UnMGG6jHyx0L=s1360-w1360-h1020",
+      "description": "A luxury hotel in New York.",
+      "rooms": {
+        "room1": { "bedSize": "Suite", "maxPepole": "single", "price": 150, "option": "Wi-Fi, Ocean View", "image": "https://lh3.googleusercontent.com/proxy/MQCob1sVBu8bOzt53rFMuisPYhhRjpJlYpDqG5T2kPjc2zzGn3kEX12AHzS1u33Fv3JJGOsxGuGu_uQEQjy0_hzTbS4IoC0udwydGI2pGTbzEHaTfq0hGD40ILLXXTxYiNjqQOdScqBGpw8hmga1-o5YEbiQTw=s1360-w1360-h1020" }
+        
+       }
+      },
+       "hotel8": {
+        "name": "Huntley Santa Monica Beach ",
+        "location": "Los Angeles",
+        "rating": "★★★★★",
+        "roomNumber": 10,
+         "map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3306.975408596148!2d-118.49858347488095!3d34.018842173169595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2a4c6829ab6fb%3A0xb7f74bc88f53f976!2sThe%20Huntley%20Hotel%20Santa%20Monica%20Beach!5e0!3m2!1sar!2seg!4v1734479244958!5m2!1sar!2seg",
+        "image": "https://lh3.googleusercontent.com/p/AF1QipPnJ53qpdasGVqi2vSpn3XMwQZj4QhqhRsF_fOX=s1360-w1360-h1020",
+        "description": "A luxury hotel in Los Angles.",
+        "rooms": {
+          "room1": { "bedSize": "Suite", "maxPepole": "single", "price": 250, "option": "Wi-Fi, Ocean View", "image": "https://lh3.googleusercontent.com/p/AF1QipOoLiKUi65uBXAuyXk_387Bdhg1pkq0nwI097p6=s1360-w1360-h1020" }
+          
+         }
+        }, 
+         "hotel9": {
+        "name": "Sonesta Los Angeles",
+        "location": "Los Angeles",
+        "rating": "★★★",
+        "roomNumber": 10,
+         "map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3309.791246269672!2d-118.38662387487719!3d33.94649687319693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2b6d46c9f6311%3A0xaaa7d114856cf27e!2z2YPYsdin2YjZhiDYqNmE2KfYstinINmE2YjYsyDYp9mG2KzZhNmK2LMg2KfZhtiq2LHZhtin2LTZitmI2YbYp9mEINin2YrYsdio2YjYsdiq!5e0!3m2!1sar!2seg!4v1734480038380!5m2!1sar!2seg",
+        "image": "https://lh3.googleusercontent.com/p/AF1QipO4fhXxmlhoxtItUx3I1dwt25XvProM43MZQHMK=s1360-w1360-h1020",
+        "description": "A luxury hotel in Los Angles.",
+        "rooms": {
+          "room1": { "bedSize": "Suite", "maxPepole": "single", "price": 250, "option": "Wi-Fi, Ocean View", "image": "https://lh3.googleusercontent.com/p/AF1QipNkYWSgqswDvBgvIgN1TCV3TqGqAduD7tb-Wz3H=s1360-w1360-h1020" }
+          
+         }
+        }, 
+
+        
+  };
+
